773d88eb447e953937a25c37298573093117e399
Jimako <janamicek@gmail.com>
2019-06-19 13:59:49 +0200

    social icons in footer

--- templates/social/social_xurl_template.php
+++ templates/social/social_xurl_template.php
@@ -9,9 +9,19 @@
 	 */
 
 
+
 	/**
 	 * {XURL_ICONS} template
 	 */
 	 $SOCIAL_XURL_TEMPLATE['default']['start'] = '<p class="xurl-social-icons hidden-print">';
 	 $SOCIAL_XURL_TEMPLATE['default']['item'] = '<a target="_blank" href="{XURL_ICONS_HREF}" data-tooltip-position="top" class="e-tip social-icon social-{XURL_ICONS_ID}" title="{XURL_ICONS_TITLE}"><span class="fa fa-fw fa-{XURL_ICONS_ID} {XURL_ICONS_CLASS}"></span></a>';
 	 $SOCIAL_XURL_TEMPLATE['default']['end'] = '</p>';
+
+	/**
+	 * {XURL_ICONS} template
+	 */
+	 
+	 $SOCIAL_XURL_TEMPLATE['footer']['start'] = '<div class="d-flex justify-content-center"><ul class="list-inline social-buttons">';
+	 $SOCIAL_XURL_TEMPLATE['footer']['item'] = ' 
+ <a class="btn btn-primary"  title="{XURL_ICONS_TITLE}"><i class="fab fa-{XURL_ICONS_ID}"></i></a> ';
+	 $SOCIAL_XURL_TEMPLATE['footer']['end'] = '</ul></div>';
