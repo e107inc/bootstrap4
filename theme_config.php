f4f8db9b6afbb3a66ea381287279013207d76867
Jimako <janamicek@gmail.com>
2019-06-19 12:16:24 +0200

    theme prefs

--- theme_config.php
+++ theme_config.php
@@ -19,7 +19,10 @@ class theme_config implements e_theme_config
 		// v2.1.4 format.
 
 		$fields = array(
-			'contact_map'  	=> array('title' => LAN_THEMEPREF_03, 'type'=>'textarea', 'writeParms'=>array('size'=>'block-level'),'help'=>''),			 
+			'map'  	=> array('title' => LAN_THEMEPREF_03, 'type'=>'textarea', 'writeParms'=>array('size'=>'block-level'),'help'=>''),	
+			'inlinecss'  	=> array('title' => LAN_THEMEPREF_01, 'type'=>'textarea', 'writeParms'=>array('size'=>'block-level'),'help'=>''),
+			'inlinejs'  	=> array('title' => LAN_THEMEPREF_02, 'type'=>'textarea', 'writeParms'=>array('size'=>'block-level'),'help'=>''),	
+			'cardmenu_look' => array('title' => LAN_THEMEPREF_04, 'type'=>'boolean', 'writeParms'=>array('default'=>1),'help'=>''),				
 		);
 
 		return $fields;
