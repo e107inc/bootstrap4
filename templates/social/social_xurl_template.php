ffac11c1a0d2baccd36aa39ec8ebbadfcdfbb240
Jimmi08 <janamicek@gmail.com>
2020-12-29 17:38:57 +0100

    added nav template

--- templates/social/social_xurl_template.php
+++ templates/social/social_xurl_template.php
@@ -17,6 +17,14 @@
  $SOCIAL_XURL_TEMPLATE['default']['item'] = '<a class="m-1 {XURL_ICONS_ID}" target="_blank" href="{XURL_ICONS_HREF}" data-tooltip-position="top" class="e-tip social-icon social-{XURL_ICONS_ID}" 
  title="{XURL_ICONS_TITLE}"><span class="e-social-{XURL_ICONS_ID} {XURL_ICONS_CLASS}"></span></a>';
  $SOCIAL_XURL_TEMPLATE['default']['end'] = '</p>';
+ 
 
- $SOCIAL_XURL_TEMPLATE['buttons']['start']  = '<div class="d-flex justify-content-center"><ul class="list-inline social-buttons">';
+ $SOCIAL_XURL_TEMPLATE['buttons']['start']  = '<div class="d-flex justify-content-center"><ul class="list-inline xurl-social-icons">';
  $SOCIAL_XURL_TEMPLATE['buttons']['item']   = '<a class="btn btn-primary {XURL_ICONS_ID} m-1"  title="{XURL_ICONS_TITLE}"><span class="e-social-{XURL_ICONS_ID} {XURL_ICONS_CLASS}"></span></a> ';
+ $SOCIAL_XURL_TEMPLATE['buttons']['end']     = '</ul></div>';
+ 
+ 
+ $SOCIAL_XURL_TEMPLATE['nav']['start']  = '<ul class="navbar-nav ml-auto nav-flex-icons xurl-social-icons">';
+ $SOCIAL_XURL_TEMPLATE['nav']['item']   = '<li class="nav-item"><a class="nav-link {XURL_ICONS_ID}"  title="{XURL_ICONS_TITLE}"><span class="e-social-{XURL_ICONS_ID} {XURL_ICONS_CLASS}"></span></a> ';
+ $SOCIAL_XURL_TEMPLATE['nav']['end']     = '</ul>'; 
+ 
